b5d5f2bd91afbe7dd5aeb225e5c682bd62714b56
Mingxun Wang <mingxun@Mingxuns-MacBook-Air.local>
2015-12-13 11:37:10 -0800

    initial commit of webserver template in python

---
+++ webserver.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, request, redirect, url_for, send_file, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def renderhomepage():
+    return render_template('homepage.html')
+
+@app.route('/testapi', methods=['GET'])
+def testapi():
+    return_obj = {}
+    return_obj["status"] = "success"
+    return json.dumps(return_obj)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
