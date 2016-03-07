2d689a38a7e0c7bcdf1b1f31dd97f7f7bf3efa5b
mingxun <mingxun@ubuntu.(none)>
2016-03-07 01:53:22 -0800

    upating the port

--- webserver.py
+++ webserver.py
@@ -40,5 +40,6 @@ def casToStructure():
     return json.dumps(return_obj)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
